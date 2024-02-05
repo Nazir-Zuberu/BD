8fe97b868bc3cd6b6c8e075ea607c7e9722ea5ea
Nazir-Zuberu <nazir.zuberu@aims-senegal.org>
2024-02-05 09:45:37 -0500

    python

---
+++ day3.py
@@ -0,0 +1,54 @@
+# print("hello world")
+
+
+
+# def addition(a,b):
+#     return(a+b)
+
+# print(addition(2,4))
+
+
+# print("""Exception handling is used to gracefully handle and recover from unexpected errors or exceptional 
+
+# conditions that may occur during program execution. It prevents the 
+# program from crashing and 
+
+# provides error-handling mechanisms. """)
+
+# print('Exception handling is used to gracefully handle and \n' 
+#       'recover romunexpected errors or exceptional conditions \n'
+#       'that may occur uring program execution. It prevents the program from crashing \n'
+#       'nd provides error-handling mechanisms.')
+
+# basket = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
+
+# for f in sorted(set(basket)):
+#     print(f)
+
+
+# for i in range(2,2):
+#     print(i)
+
+
+
+# for n in range(2, 10):
+#     for x in range(2, n):
+#         if n % x == 0:
+#             print(n, 'equals', x, '*', n//x)
+#             break
+#     else:
+#         # loop fell through without finding a factor
+#         print(n, 'is a prime number')
+
+
+# for n in range(2, 10):
+#     print(n)
+
+
+
+a = [2,45,66,7,8,9]
+
+
+b = [0,0]
+
+print(a+b)
