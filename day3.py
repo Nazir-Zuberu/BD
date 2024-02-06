5ddb6bddaeef460d9259a828dbd02a4aaf205b16
Nazir-Zuberu <nazir.zuberu@aims-senegal.org>
2024-02-06 05:55:15 -0500

    scala folder added

--- day3.py
+++ day3.py
@@ -45,9 +45,32 @@
 #     print(n)
 
 
-a = [2,45,66,7,8,9]
+# a = [2,45,66,7,8,9]
 
 
-b = [0,0]
+# b = [0,0]
 
-print(a+b)
+# print(a+b)
+
+
+
+# def fibonacci(n):
+#     if n<=0:
+#         return 0
+#     elif n==1:
+#         return 1
+#     else:
+#         return fibonacci(n-2) + fibonacci(n-1)
+
+# print(fibonacci(5))
+
+
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+]
+
+new1 = [ [row[i] for row in matrix] for i in range(4)]
+
+print(new1)
